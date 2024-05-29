eb99a50b2b44994d3d0346ba05b86697142f3933
pmammedov <pmammedov@gmail.com>
2024-05-29 14:54:27 +0200

    chore: Update app.json and package.json for Aora app

--- Aora/app/(auth)/_layout.jsx
+++ Aora/app/(auth)/_layout.jsx
@@ -1,14 +1,17 @@
-import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { Stack } from 'expo-router'
+import { StatusBar } from 'expo-status-bar'
 
-const AppLayout = () => {
+const AuthLayout = () => {
   return (
-    <View>
-      <Text>AppLayout</Text>
-    </View>
+    <>
+      <Stack>
+        <Stack.Screen name="sign-in" options={{ headerShown: false }} />
+        <Stack.Screen name="sign-up" options={{ headerShown: false }} />
+        <StatusBar backgroundColor='#161622' style='light' />
+      </Stack>
+    </>
   )
 }
 
-export default AppLayout
-
-const styles = StyleSheet.create({})
+export default AuthLayout
--- Aora/app/index.jsx
+++ Aora/app/index.jsx
@@ -1,14 +1,31 @@
-import { Link } from 'expo-router';
+import { Redirect, router } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
-import {  Text, View } from 'react-native';
+import { Image, ScrollView, Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { images } from '../constants'
+import CustomButton from '../components/CustomButton';
+import 'react-native-url-polyfill/auto'
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className='font-pblack'>this is Index </Text>
-          <StatusBar />
-          <Link href='/home' style={{color:'blue'}}>Go to Home</Link>
-    </View>
+    <SafeAreaView className='bg-primary h-full'>
+      <ScrollView contentContainerStyle={{ height: '100%' }}>
+        <View className='w-full items-center justify-center min-h-[85vh] px-4'>
+          <Image source={images.logo} className='w-[130px] h-[84px]' resizeMode='contain' />
+          <Image source={images.cards} className='max-w-[380px] w-full h-[300px]' resizeMode='contain' />
+          <View className='relative mt-5'>
+            <Text className='text-4xl text-white font-bold text-center'>Discover Endless Possibilities With{' '}
+              <Text className='text-secondary-200'>Aora</Text>
+            </Text>
+            <Image source={images.path} className='w-[136px] h-[15px] absolute -bottom-2 -right-8' resizeMode='contain' />
+          </View>
+          <Text className='text-base font-pregular text-gray-100 text-center mt-7'>Where creativity meets innovation: embark on a journey of limitless explorarionwith Aora</Text>
+          <CustomButton title='Continue with Email' containerStyles='w-full mt-7' handlePress={()=>{router.push('/sign-in')}} />
+        </View>
+      </ScrollView>
+      <StatusBar style="light" backgroundColor='#161622' />
+
+    </SafeAreaView>
   );
 }
 
